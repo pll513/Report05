71fb8f78f170539655421cb91dc533367922b03d
PuAnfeng <paf32@qq.com>
2015-06-18 12:44:47 +0800

    fix a mistake in rho.m

--- Lab05.m
+++ Lab05.m
@@ -1,3 +1,18 @@
 %Lab05
 clear; clc;
-H1 = generateMatrixHilbert(2);
+
+N = [2,3,4,5,6];
+W = zeros(1,5);
+R = zeros(1,5);
+w = linspace(0,2,10000);
+
+for i = 1:5
+    H = generateMatrixHilbert(N(i));
+    rho = rho(H,w);
+    W(i) = w(find(rho==min(rho),1));
+    R(i) = min(rho);
+end
+disp(W);
+disp(R);
+
+
